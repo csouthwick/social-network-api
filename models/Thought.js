ba81545a2f02514f87bb4254af5bd962eba805d9
Chris Southwick <csouthwick@live.com>
2020-12-13 23:20:37 -0500

    add reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,33 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => moment(createdAtVal).format('MMM Do, YYYY at h:mm a')
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
 
 const ThoughtSchema = new Schema(
   {
@@ -18,12 +46,7 @@ const ThoughtSchema = new Schema(
       type: String,
       required: true
     },
-    reactions: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Reaction'
-      }
-    ]
+    reactions: [ReactionSchema]
   },
   {
     toJSON: {
