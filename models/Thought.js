315f3802d3dd47f0ed192358c34c689bcb1b2fe6
Chris Southwick <csouthwick@live.com>
2020-12-13 23:13:39 -0500

    add Thought model and moment.js

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => moment(createdAtVal).format('MMM Do, YYYY at h:mm a')
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Reaction'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
